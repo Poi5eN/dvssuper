43b27e2a697e06edc9e68049ba5a9b0bd43a7ba4
Poi5eN <gaurav.upadhyay.vasudeva@gmail.com>
2025-12-27 20:17:53 +0530

    test code

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { PostHogProvider } from "posthog-js/react";
 
-createRoot(document.getElementById('root')).render(
+const options = {
+  api_host: "https://eu.i.posthog.com",
+};
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_POSTHOG_KEY}
+      options={options}
+    >
+      <App />
+    </PostHogProvider>
+  </StrictMode>
+);
